2253b63a8b19129fc07abb42f00e67a76f038c04
HarutoKoike <koike@kugi.kyoto-u.ac.jp>
2022-11-24 02:05:21 +0900

    modified:   dd6_setup.sh

--- dd6_setup.sh
+++ dd6_setup.sh
@@ -5,8 +5,8 @@ echo export DATA_PATH=~/data >> ~/.bashrc
 echo export IDL_STARTUP=~/.idl/idl_startup.pro >> ~/.bashrc
 echo export IDL_PATH='<IDL_DEFAULT>:+~/.idl'   >> ~/.bashrc
 
-touch csa_info.csv
-echo DEVICE, DECOMPOSED=0 \\nLOADCT, 39 \\n!P.BACKGROUND=255 \\n!P.COLOR=0 > idl_startup.pro
+touch ~/.idl/csa_info.csv
+echo DEVICE, DECOMPOSED=0 \\nLOADCT, 39 \\n!P.BACKGROUND=255 \\n!P.COLOR=0 > ~/.idl/idl_startup.pro
 
 
 sudo mv /usr/local/itt/idl/idl80/bin/bin.linux.x86_64/libcurl.so /usr/local/itt/idl/idl80/bin/bin.linux.x86_64/libcurl_old.so
