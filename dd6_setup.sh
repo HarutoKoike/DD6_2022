49dc0519b9eded3ab1bf26044c409d1ec5a79a3d
HarutoKOIKE <koike@kugi.kyoto-u.ac.jp>
2022-11-24 22:42:41 +0900

    modified:   dd6_setup.sh

--- dd6_setup.sh
+++ dd6_setup.sh
@@ -5,7 +5,7 @@ echo export DATA_PATH=~/data >> ~/.bashrc
 echo export IDL_STARTUP=~/.idl/idl_startup.pro >> ~/.bashrc
 echo export IDL_PATH='<IDL_DEFAULT>:+~/.idl'   >> ~/.bashrc
 
-echo DEVICE, DECOMPOSED=0 \\nLOADCT, 39 \\n!P.BACKGROUND=255 \\n!P.COLOR=0 > ~/.idl/idl_startup.pro
+cp idl_startup.pro ~/.idl
 
 
 sudo mv /usr/local/itt/idl/idl80/bin/bin.linux.x86_64/libcurl.so /usr/local/itt/idl/idl80/bin/bin.linux.x86_64/libcurl_old.so
--- lib/mission/dmsp/background/dmsp::file_test.pro
+++
@@ -1,4 +0,0 @@
-FUNCTION dmsp::file_test
-fn = self->filename()
-RETURN, FILE_TEST(fn)
-END
